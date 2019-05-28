99860c89a7ae581371202b8c75c47dd0669913b7
lsa77 <lukasz.sawicki.2@gmail.com>
2019-05-29 00:10:05 +0200

    fix flask app router - bad method b64encode

--- router/router.py
+++ router/router.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 payload = '<?xml version="1.0" encoding="utf-8"?><asx:abap version="1.0" xmlns:asx="http://www.sap.com/abapxml"><asx:values><DATA><KBJPR>JPK</KBJPR><EXPDT>2020-04-28</EXPDT><SAPCK>P0203380088</SAPCK><SAPIN>0120034711</SAPIN><TAXID>PL7691967641</TAXID><CRDAT>2019-04-29</CRDAT><REFNR/><CMPNM>TERMOKOR KAEFER Sp. z o.o.</CMPNM><LNOTE/></DATA></asx:values></asx:abap>'
 
 
-@app.route('/b64encode')
-def hello_world():
+@app.route('/b64encode', methods=['POST'])
+def post_b64encode():
 
     url = 'ejpk-backend-signer-service.default.svc.cluster.local:80/b64encode'
     headers = {'Content-Type': 'text/xml'}
