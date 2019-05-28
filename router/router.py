4e355a0fb745b1a7048eeabbec0d72ed9a6df43c
lsa77 <lukasz.sawicki.2@gmail.com>
2019-05-28 22:35:37 +0200

    router actually routes

--- router/router.py
+++ router/router.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
+payload = '<?xml version="1.0" encoding="utf-8"?><asx:abap version="1.0" xmlns:asx="http://www.sap.com/abapxml"><asx:values><DATA><KBJPR>JPK</KBJPR><EXPDT>2020-04-28</EXPDT><SAPCK>P0203380088</SAPCK><SAPIN>0120034711</SAPIN><TAXID>PL7691967641</TAXID><CRDAT>2019-04-29</CRDAT><REFNR/><CMPNM>TERMOKOR KAEFER Sp. z o.o.</CMPNM><LNOTE/></DATA></asx:values></asx:abap>'
 
-@app.route('/')
+
+@app.route('/b64encode')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+
+    url = '"ejpk-backend-signer-service.default.svc.cluster.local:80/b64encode"'
+    headers = {'content-type': 'text/xml'}
+
+
+    response = requests.post(url=url, headers=headers, data=payload   )
+
+    print(response)
+
+    return response.content
 
 
 if __name__ == '__main__':
